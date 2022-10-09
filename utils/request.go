145e2d2498620e5cf2562323556647455c35ba97
n3xtchen <echenwen@gmail.com>
2022-10-09 19:32:15 +0800

    request 加入可变参数

--- utils/request.go
+++ utils/request.go
@@ -23,34 +23,75 @@ type Auth struct {
     password string
 }
 
-func NewAuth(user, password string) *Auth {
-    return &Auth{user: user, password: password}
+type RequestOptions struct {
+    data io.Reader
+    params map[string]string
+    headers map[string]string
+    auth *Auth
 }
 
-func do(method string, url string, data io.Reader, params map[string]string, header map[string]string, auth *Auth) (*http.Response, error) {
+type ModifyOptFunc func(opt *RequestOptions)
 
-    req, err := http.NewRequest(method, url, data)
+func WithData(data io.Reader) ModifyOptFunc {
+    return func(opts *RequestOptions) {
+        opts.data = data
+    }
+}
+
+func WithParams(params map[string]string) ModifyOptFunc {
+    return func(opts *RequestOptions) {
+        opts.params = params
+    }
+}
+
+func WithHeaders(headers map[string]string) ModifyOptFunc {
+    return func(opts *RequestOptions) {
+        opts.headers = headers
+    }
+}
+
+func WithAuth(user, password string) ModifyOptFunc {
+    return func(opts *RequestOptions) {
+        opts.auth =  &Auth{user: user, password: password}
+    }
+}
+
+// func do(method string, url string, data io.Reader, params map[string]string, headers map[string]string, auth *Auth) (*http.Response, error) {
+func do(method string, url string, opts ...ModifyOptFunc) (*http.Response, error) {
+
+    rOpts := RequestOptions{
+        params: nil,
+        data: nil,
+        headers: nil,
+        auth: nil,
+    }
+
+    for _, fun := range opts {
+        fun(&rOpts)
+    }
+
+    req, err := http.NewRequest(method, url, rOpts.data)
     if err != nil {
         log.Println("request failed", err)
         return nil, err
     }
 
     // header
-    if header != nil {
-        for key, val := range header {
+    if rOpts.headers != nil {
+        for key, val := range rOpts.headers {
             req.Header.Add(key, val)
         }
     }
 
     // auth
-    if auth != nil {
-        req.SetBasicAuth(auth.user, auth.password)
+    if rOpts.auth != nil {
+        req.SetBasicAuth(rOpts.auth.user, rOpts.auth.password)
     }
 
     // querystring
-    if params != nil {
+    if rOpts.params != nil {
         q := req.URL.Query()
-        for key, val := range params {
+        for key, val := range rOpts.params {
             q.Add(key, val)
         }
         req.URL.RawQuery = q.Encode()
@@ -63,7 +104,7 @@ func do(method string, url string, data io.Reader, params map[string]string, hea
     return response, nil
 }
 
-func Get(url string, params map[string]string, header map[string]string, auth *Auth) (*http.Response, error) {
-    return do("GET", url, nil, params, header, auth)
+func Get(url string, opts ...ModifyOptFunc) (*http.Response, error) {
+    return do("GET", url, opts...)
 }
 
--- utils/request_test.go
+++ utils/request_test.go
@@ -7,10 +7,7 @@ import (
 )
 
 func TestEncoding(t *testing.T) {
-    auth := NewAuth("qADsi&JR","9969B926-B56E-79F3-4516-0585A4647EDB")
-    fmt.Println(auth)
-
-    response, _ := Get("https://baidu.com", nil, nil, nil)
+    response, _ := Get("https://baidu.com")
     fmt.Println(response)
 }
 
