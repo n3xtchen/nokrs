05a331bc7a7e78fcadb97ede5f4799df17c149ea
n3xtchen <echenwen@gmail.com>
2023-03-13 17:22:27 +0800

    调整项目结构

---
+++ internal/tapd/builder.go
@@ -0,0 +1,14 @@
+package tapd
+
+type Builder struct {
+    user string
+    password string
+    User Tapd[UserResp]
+    Story Tapd[StoryResp]
+}
+
+func With(user, password string) *Tapd[any] {
+    return NewTapd[UserResp](user, password, "workspaces/users")
+}
+
+
