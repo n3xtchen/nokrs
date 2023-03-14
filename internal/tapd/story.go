710fcdd7e25992df7b6fb5691aeeebb7ff0d5c75
n3xtchen <echenwen@gmail.com>
2023-03-14 15:21:53 +0800

    tapd: 读取迭代信息和任务信息

--- internal/tapd/story.go
+++ internal/tapd/story.go
@@ -3,7 +3,18 @@ package tapd
 
 type StoryData []map[string]struct {
     Id string `json:"id"`
+    ParentId string `json:"parent_id"`
+    Status string `json:"status"`
     Name string `json:"name"`
+    Description string `json:"description"`
+    Owner string `json:"owner"`
+    Creator string `json:"creator"`
+    Developer string `json:"developer"`
+    Created string `json:"created"`
+    Begin string `json:"begin"`
+    Due string `json:"due"`
+    Completed string `json:"completed"`
+    IterationId string `json:"iteration_id"`
 }
 
 type StoryResp struct {
--- internal/tapd/story_test.go
+++ internal/tapd/story_test.go
@@ -11,12 +11,16 @@ func TestStory(t *testing.T) {
     tapd.SetParams(map[string]string {
         "workspace_id": tapdWorkSpace,
         "iteration_id": tapdIterationId,
-        "fields": "id,name",
+        // "fields": "id,name",
     })
     tapd.fetch()
     result, _ := tapd.toObj()
     for _, val := range result.Data {
-        fmt.Println(val)
+        fmt.Println(val["Story"].Name)
+        fmt.Println(val["Story"].Creator)
+        fmt.Println(val["Story"].Owner)
+        fmt.Println(val["Story"].ParentId)
+        break
     }
 }
 
