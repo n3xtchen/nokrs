30e2ab2bbfeca08b381c7a65e10643bf7ae61ef7
n3xtchen <echenwen@gmail.com>
2023-03-16 12:24:01 +0800

    前端界面：代码清理

--- static/components/Objectives.jsx
+++ static/components/Objectives.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Objective from './Objective';
 
 const Objectives = ({ objectives, setObjectives }) => {
-	return (
-		<div>
-			<h1 className="text-2xl font-bold mb-4">Objectives</h1>
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Objectives</h1>
       <ul>
-        {objectives.map((objective) => (
-          <Objective
-            key={objective.id}
-            objective={objective}
-            setObjectives={setObjectives}
-            objectives={objectives}
-          />
-        ))}
+      {objectives.map((objective) => (
+        <Objective
+          key={objective.id}
+          objective={objective}
+          setObjectives={setObjectives}
+          objectives={objectives}
+        />
+      ))}
       </ul>
     </div>
   );
